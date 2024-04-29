ecf64c4857c63fc165fefc59ef539912a6b3f38b
ahmed <ahmedsaheed2@outlook.com>
2024-04-29 20:39:45 +0100

    feat(core): add character animation
    1. Setup idle state animation
    2. Move state animation

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -7,6 +7,7 @@ using UnityEngine;
 public class Player : MonoBehaviour {
     
     [SerializeField] private float moveSpeed = 7f;
+    private bool isWalking;
     private void Update() {
         Vector2 inputVector = new Vector2(0, 0);
         if (Input.GetKey(KeyCode.W)) {
@@ -25,7 +26,12 @@ public class Player : MonoBehaviour {
 
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
+        isWalking = moveDir != Vector3.zero;
         float rotationSpeed = 10f;
         transform.forward =Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotationSpeed);
     }
+    
+    public bool IsWalking() {
+        return isWalking;
+    } 
 }
