f69e206cfcc8f6fdeb2a0608aea8fae4680fd3a2
ahmed <ahmedsaheed2@outlook.com>
2024-04-30 03:51:12 +0100

    feat(core): add selected counter visuals

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,38 +5,43 @@ using UnityEngine;
 
 
 public class Player : MonoBehaviour {
+    public static Player Instance { get; private set; }
+
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+    public class OnSelectedCounterChangedEventArgs : EventArgs {
+        public ClearCounter selectedCounter;
+    }
+
     [SerializeField] private float moveSpeed = 7f;
     [SerializeField] private GameInput gameInput;
     [SerializeField] private LayerMask counterLayerMask;
 
     private bool isWalking;
     private Vector3 lastInteractionDirection;
+    private ClearCounter selectedCounter;
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("Multiple Player instance in the scene");
+        }
+
+        Instance = this;
+    }
 
     private void Start() {
         gameInput.OnInteractAction += GameInput_OnInteractAction;
     }
 
-    private void GameInput_OnInteractAction(object sender, EventArgs e) {
-        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
-        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
-
-        if (moveDir != Vector3.zero) lastInteractionDirection = moveDir;
-
-        float interactionDistance = 2f;
-        // we are looking objects that has a physic collider in the direction of the last movement
-        if (Physics.Raycast(transform.position, lastInteractionDirection, out RaycastHit raycastHit,
-                interactionDistance, counterLayerMask)) {
-            // we found an object, is it a ClearCounter 
-            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
-                // its a ClearCounter, proceed to interact   
-                clearCounter.Interact();
-            }
-        }
-    }
-
     private void Update() {
         MovementHandler();
         InteractionHandler();
+    }
+
+    private void GameInput_OnInteractAction(object sender, EventArgs e) {
+        if (selectedCounter != null) {
+            selectedCounter.Interact();
+        }
     }
 
     private void InteractionHandler() {
@@ -52,8 +57,16 @@ public class Player : MonoBehaviour {
             // we found an object, is it a ClearCounter 
             if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter)) {
                 // its a ClearCounter, proceed to interact   
-                // clearCounter.Interact();
+                if (clearCounter != selectedCounter) {
+                    SetSelectedCounter(clearCounter);
+                }
             }
+            else {
+                SetSelectedCounter(null);
+            }
+        }
+        else {
+            SetSelectedCounter(null);
         }
     }
 
@@ -108,5 +121,12 @@ public class Player : MonoBehaviour {
 
     public bool IsWalking() {
         return isWalking;
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter) {
+        this.selectedCounter = selectedCounter;
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs {
+            selectedCounter = selectedCounter
+        });
     }
 }
