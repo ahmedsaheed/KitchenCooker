bde135a18eb877b7929f1c0ef3ecbac3b9eed91c
ahmed <ahmedsaheed2@outlook.com>
2024-05-01 00:48:52 +0100

    feat(core): add spawning of kitchen objects on the kitchen counter and parent counter detection

--- Assets/Scripts/ClearCounter.cs
+++ Assets/Scripts/ClearCounter.cs
@@ -4,11 +4,44 @@ public class ClearCounter : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
     [SerializeField] private Transform counterTopPoint;
+    [SerializeField] private ClearCounter secondClearCounter;
+    [SerializeField] private bool testing;
+    private KitchenObject kitchenObject;
+
+    private void Update() {
+        if (testing && Input.GetKeyDown(KeyCode.T))
+            if (kitchenObject != null)
+                kitchenObject.SetClearCounter(secondClearCounter);
+    }
 
     public void Interact() {
-        Debug.Log("Interacting with ClearCounter");
-        var kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
-        kitchenObjectTransform.localPosition = Vector3.zero;
-        Debug.Log(kitchenObjectTransform.GetComponent<KitchenObject>().GetKitchenObjectSO().objectName);
+        if (kitchenObject == null) {
+            var kitchenObjectTransform = Instantiate(kitchenObjectSO.prefab, counterTopPoint);
+            kitchenObjectTransform.GetComponent<KitchenObject>().SetClearCounter(this);
+        }
+        else {
+            Debug.Log(kitchenObject.GetClearCounter());
+        }
+    }
+
+    public Transform GetKitchenObjectFollowTransform() {
+        return counterTopPoint;
+    }
+
+
+    public void SetKitchenObject(KitchenObject Ko) {
+        kitchenObject = Ko;
+    }
+
+    public KitchenObject GetKitchenObject() {
+        return kitchenObject;
+    }
+
+    public void ClearKitchenObject() {
+        kitchenObject = null;
+    }
+
+    public bool HasKitchenObject() {
+        return kitchenObject != null;
     }
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -3,8 +3,25 @@ using UnityEngine;
 public class KitchenObject : MonoBehaviour {
     // Start is called before the first frame update
     [SerializeField] private KitchenObjectScriptableObject kitchenObjectSO;
+    private ClearCounter clearCounter;
 
     public KitchenObjectScriptableObject GetKitchenObjectSO() {
         return kitchenObjectSO;
+    }
+
+    public void SetClearCounter(ClearCounter clearCounter) {
+        if (this.clearCounter != null) this.clearCounter.ClearKitchenObject();
+
+        this.clearCounter = clearCounter;
+        if (clearCounter.HasKitchenObject()) {
+            Debug.Log("Current counter has kitchen object!");
+        }
+        clearCounter.SetKitchenObject(this);
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() {
+        return clearCounter;
     }
 }
