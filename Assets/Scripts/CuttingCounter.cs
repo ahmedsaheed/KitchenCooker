8ab439ab2058610ad8b1ce06046ac90b9c444d3d
ahmed <ahmedsaheed2@outlook.com>
2024-05-03 15:03:35 +0100

    feat(core): add ability to cut multiple dynamic cuttable kitchen object
    * Cabbage Cutting
    * Tomato Cutting
    * Cheese Cutting

--- Assets/Scripts/CuttingCounter.cs
+++ Assets/Scripts/CuttingCounter.cs
@@ -1,30 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class CuttingCounter : BaseCounter {
-   
-   [SerializeField] private KitchenObjectScriptableObject cutKitchenObjectSO;
-   public override void Interact(Player player) {
-      if (!HasKitchenObject()) {
-         if (player.HasKitchenObject()) {
-            player.GetKitchenObject().SetKitchenObjectParent(this);
-         }
-      }
-      else {
-         if (player.HasKitchenObject()) { }
-         else {
-            GetKitchenObject().SetKitchenObjectParent(player);
-         }
+    [SerializeField] private CuttingRecipeSO[] cuttingRecipeSoArray;
 
-      }
-   }
-   
-   public override void InteractAlternate(Player player) {
-      if (HasKitchenObject()) {
-         // destroy object and spawn cut version of the object
-         GetKitchenObject().DestroySelf();
-         KitchenObject.SpawnKitchenObject(cutKitchenObjectSO, this);
-      }
-   }
+    public override void Interact(Player player) {
+        if (!HasKitchenObject()) {
+            if (player.HasKitchenObject())
+                if (HasRecipeWithInput(player.GetKitchenObject().GetKitchenObjectSO()))
+                    player.GetKitchenObject().SetKitchenObjectParent(this);
+        }
+        else {
+            if (player.HasKitchenObject()) { }
+            else {
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
+    }
+
+    public override void InteractAlternate(Player player) {
+        if (HasKitchenObject() && HasRecipeWithInput(GetKitchenObject().GetKitchenObjectSO())) {
+            // destroy object and spawn cut version of the object
+            var outputKitchenObjectSO = GetOutputForInput(GetKitchenObject().GetKitchenObjectSO());
+            GetKitchenObject().DestroySelf();
+            KitchenObject.SpawnKitchenObject(outputKitchenObjectSO, this);
+        }
+    }
+
+    private KitchenObjectScriptableObject GetOutputForInput(
+        KitchenObjectScriptableObject inputKitchenObjectScriptableObject) {
+        foreach (var cuttingRecipeSo in cuttingRecipeSoArray)
+            if (cuttingRecipeSo.input == inputKitchenObjectScriptableObject)
+                return cuttingRecipeSo.output;
+
+        return null;
+    }
+
+    private bool HasRecipeWithInput(KitchenObjectScriptableObject inputKitchenObjectScriptableObject) {
+        foreach (var cuttingRecipeSo in cuttingRecipeSoArray)
+            if (cuttingRecipeSo.input == inputKitchenObjectScriptableObject)
+                return true;
+        return false;
+    }
 }
---
+++ Assets/Scripts/CuttingRecipeSO.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu]
+public class CuttingRecipeSO : ScriptableObject {
+    public KitchenObjectScriptableObject input;
+    public KitchenObjectScriptableObject output;
+}
